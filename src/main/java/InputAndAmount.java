56dda0cabd69bfe23e3741613ceec32180e87eec
polrouter <egor.pavelyev@rambler.ru>
2017-03-22 21:34:04 +0500

    new commit

---
+++ src/main/java/InputAndAmount.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class InputAndAmount {
+    public static void main(String[] args) {
+
+        System.out.println("Add two numbers");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        in.close();
+//
+        int sum = a + b;
+        System.out.println("Result a + b = " + sum);
+
+
+        System.out.println("End");
+    }
+
+
+
+}
+
+
