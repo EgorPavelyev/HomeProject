ad1026706e2f44ace1aaaabe8d09da0b5832cb59
polrouter <egor.pavelyev@rambler.ru>
2017-04-01 21:26:48 +0500

    Add Button

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -14,6 +14,21 @@ public class Calculator extends JFrame {
 
         JTextArea textArea = new JTextArea();
 
+        JPanel buttonPanel = new JPanel(new GridLayout(3,5));
+
+        JButton button0 = new JButton("0");
+        JButton button1 = new JButton("1");
+        JButton button2 = new JButton("2");
+        JButton button3 = new JButton("3");
+        JButton button4 = new JButton("4");
+        JButton button5 = new JButton("5");
+        JButton button6 = new JButton("6");
+        JButton button7 = new JButton("7");
+        JButton button8 = new JButton("8");
+        JButton button9 = new JButton("9");
+
+
+
 
 //        System.out.println("Add two numbers");
 //        Scanner in = new Scanner(System.in);
