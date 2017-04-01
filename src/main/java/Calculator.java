8c6c66bd0d6faf54cdf1985ed954efb839d69b01
polrouter <egor.pavelyev@rambler.ru>
2017-04-01 21:16:38 +0500

    Add Frame

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Calculator extends JFrame {
+    Calculator() {
+        setBounds(300, 300, 300, 300);
+        setLayout(new BorderLayout());
+        setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+        new Calculator();
+
+        JTextArea textArea = new JTextArea();
+
+
+//        System.out.println("Add two numbers");
+//        Scanner in = new Scanner(System.in);
+//        int a = in.nextInt();
+//        int b = in.nextInt();
+//        in.close();
+//        int sum = a + b;
+//        System.out.println("Result a + b = " + sum);
+//        System.out.println("End");
+
+    }
+
+
+}
+
+
